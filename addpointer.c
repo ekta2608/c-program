975678037cefd994969a13077a5f4d57bc5766d9
ekta2608 <112929206+ekta2608@users.noreply.github.com>
2023-05-12 11:14:11 +0530

    Add files via upload

---
+++ addpointer.c
@@ -0,0 +1,20 @@
+// addition with pointer
+
+#include<stdio.h>
+
+main()
+{
+	float a=12.23,b=12.45,*p,*q,sum;
+	p=&a;
+	q=&b;
+	
+	printf("%f + %f = %f",a,b,(a+b));
+	//scanf("%f %f",&a,&b);
+	
+	//sum=*p + *q;
+	
+	//printf("sum of two values :  %.2lf",sum=*p + *q);
+	
+	
+	
+}
